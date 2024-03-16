3145201b3deed2032d588f53723ebae05364e7d3
RichardKaranuMbuti <Officialforrichardk@gmail.com>
2024-03-16 13:35:15 +0300

    Add cython to dependecies

--- build-linux-wheels.sh
+++ build-linux-wheels.sh
@@ -3,8 +3,8 @@
 # Install dependencies inside the Docker container
 yum install -y python3 python3-pip
 
-# Install wheel and setuptools
-pip3 install wheel setuptools
+# Install wheel, setuptools, and Cython
+pip3 install wheel setuptools Cython
 
 # Build the wheels
 for PYBIN in /opt/python/cp3{6,7,8,9,10}*/bin; do
