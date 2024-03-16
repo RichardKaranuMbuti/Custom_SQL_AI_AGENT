4b8b146b81403a98c26f3e340fe844138330016d
RichardKaranuMbuti <Officialforrichardk@gmail.com>
2024-03-16 13:32:13 +0300

    Add cython to dependecies

--- build-linux-wheels.sh
+++ build-linux-wheels.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-# Compile wheels
-for PYBIN in /opt/python/cp3{6,7,8,9}*/bin; do
-    "${PYBIN}/pip" wheel /io/ -w /io/dist/
+# Install dependencies inside the Docker container
+yum install -y python3 python3-pip
+
+# Install wheel and setuptools
+pip3 install wheel setuptools
+
+# Build the wheels
+for PYBIN in /opt/python/cp3{6,7,8,9,10}*/bin; do
+    "${PYBIN}/python" -m pip install -r /io/requirements.txt
+    "${PYBIN}/pip" wheel /io/ -w /io/wheelhouse/
 done
 
-# Bundle external shared libraries into the wheels
-for whl in /io/dist/*.whl; do
-    auditwheel repair "$whl" -w /io/dist/
+# Bundle the wheel files
+for whl in /io/wheelhouse/*.whl; do
+    auditwheel repair "$whl" --plat manylinux2014_x86_64 -w /io/wheelhouse/
 done
 
-# Set permissions to allow uploading to PyPI
-chmod 666 /io/dist/*
+# Install the repaired wheels using pip
+pip3 install /io/wheelhouse/*.whl
