4282eb51051bfaa167f65f5cd6dcf2dfef9f7912
RichardKaranuMbuti <Officialforrichardk@gmail.com>
2024-03-17 05:41:46 +0300

    Update to build wheels using docker

---
+++ build_wheels.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e -x
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    if [[ ${PYBIN} == *"cp38"* ]]; then  # Adjust according to your needs
+        "${PYBIN}/pip" install -U pip setuptools wheel Cython
+        "${PYBIN}/pip" wheel /github/workspace/ --no-deps -w wheelhouse/
+    fi
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /github/workspace/dist/
+done
+
+# Fix permissions of the wheels so the host can clean them up
+chmod -R a+rwX /github/workspace/dist/
+
