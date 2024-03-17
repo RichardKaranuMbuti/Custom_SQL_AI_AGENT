8072fb437361571cabde6bbeb14d4bce83f54bbd
RichardKaranuMbuti <Officialforrichardk@gmail.com>
2024-03-17 05:26:32 +0300

    create new setup and workflows

--- setup.py
+++ setup.py
@@ -1,27 +1,50 @@
+from setuptools import setup, find_packages, Extension
+from Cython.Build import cythonize
 import os
-from setuptools import setup, find_packages
+from os.path import join
+import glob
 
 # Directory containing your miksisdk package
 directory_path = os.path.dirname(os.path.abspath(__file__))
+miksisdk_path = join(directory_path, 'miksi_ai_sdk')
+
+# Function to get all .py files except __init__.py from miksisdk directory
+def get_py_files(directory):
+    return [f for f in glob.glob(join(directory, '**/*.py'), recursive=True) 
+            if not f.endswith('__init__.py')]
+
+# Get all .py files to be cythonized
+miksisdk_files = get_py_files(miksisdk_path)
+
+# Create extensions for each .py file
+extensions = [
+    Extension(
+        name=os.path.splitext(os.path.relpath(file, directory_path))[0].replace(os.path.sep, '.'),
+        sources=[file],
+        include_dirs=[directory_path]  # Ensure this is adjusted or removed based on your needs
+    ) for file in miksisdk_files
+]
 
 setup(
     name="miksi_ai_sdk",
     version="0.0.5",
+    author="RichardKaranuMbuti",
+    author_email="officialforrichardk@gmail.com",
     description="Miksi-AI empowers your BI",
     long_description=open(os.path.join(directory_path, 'docs.md')).read(),
     long_description_content_type='text/markdown',
     url="https://github.com/Miksi-io/Custom-Agent",
-    author="RichardKaranuMbuti",
-    author_email="officialforrichardk@gmail.com",
     packages=find_packages(),
+    ext_modules=cythonize(extensions, language_level="3"),
     install_requires=[
-        "langchain", "sqlalchemy", "pymysql",
+        "Cython", "langchain", "sqlalchemy", "pymysql",
         "langchain_openai", "openai"
     ],
     python_requires='>=3.6',
+    classifiers=[
+        # Add all relevant classifiers here
+    ],
 )
-
-
 
 '''
 import os
