ea442a75cedbcc1079d9b5ece6d346665ea07e0e
RichardKaranuMbuti <Officialforrichardk@gmail.com>
2024-03-18 13:32:01 +0300

    Create  new yml workflow

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from setuptools import setup, find_packages, Extension
-from Cython.Build import cythonize
+from setuptools import setup, find_packages
 import os
 from os.path import join
 import glob
@@ -13,17 +12,8 @@ def get_py_files(directory):
     return [f for f in glob.glob(join(directory, '**/*.py'), recursive=True)
             if not f.endswith('__init__.py')]
 
-# Get all .py files to be cythonized
+# Get all .py files
 miksisdk_files = get_py_files(miksisdk_path)
-
-# Create extensions for each .py file
-extensions = [
-    Extension(
-        name=os.path.splitext(os.path.relpath(file, directory_path))[0].replace(os.path.sep, '.'),
-        sources=[file],
-        include_dirs=[directory_path]  # Ensure this is adjusted or removed based on your needs
-    ) for file in miksisdk_files
-]
 
 setup(
     name="miksi_ai_sdk",
@@ -34,22 +24,15 @@ setup(
     long_description=open(os.path.join(directory_path, 'docs.md')).read(),
     long_description_content_type='text/markdown',
     url="https://github.com/Miksi-io/Custom-Agent",
-    packages=find_packages(),
-    ext_modules=cythonize(extensions, language_level="3"),
+    packages=find_packages(include=["miksi_ai_sdk", "miksi_ai_sdk.*"]),
+    py_modules=["miksi_ai_sdk." + os.path.splitext(os.path.basename(f))[0] for f in miksisdk_files],
     install_requires=[
-        "Cython", "langchain", "sqlalchemy", "pymysql",
+        "langchain", "sqlalchemy", "pymysql",
         "langchain_openai", "openai"
     ],
     python_requires='>=3.6',
     classifiers=[
-        # Add all relevant classifiers here
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        # Add your classifiers here
     ],
 )
 
