f9cd4df665ec3a5588727f3cc7a5aa256c681745
RichardKaranuMbuti <Officialforrichardk@gmail.com>
2024-03-18 20:01:37 +0300

    Create  new yml workflow

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup, find_packages, Extension
 from Cython.Build import cythonize
 import os
-from os.path import join
 import glob
 
 # Directory containing your miksisdk package
 directory_path = os.path.dirname(os.path.abspath(__file__))
-miksisdk_path = join(directory_path, 'miksi_ai_sdk')
+miksisdk_path = os.path.join(directory_path, 'miksi_ai_sdk')
 
 # Function to get all .pyx files from miksisdk directory
 def get_pyx_files(directory):
-    return [f for f in glob.glob(join(directory, '**/*.pyx'), recursive=True)]
+    return [f for f in glob.glob(os.path.join(directory, '**/*.pyx'), recursive=True)]
 
 # Get all .pyx files
 miksisdk_files = get_pyx_files(miksisdk_path)
@@ -23,7 +22,7 @@ extensions = [
 
 setup(
     name="miksi_ai_sdk",
-    version="0.0.10",
+    version="0.0.11",
     author="RichardKaranuMbuti",
     author_email="officialforrichardk@gmail.com",
     description="Miksi-AI empowers your BI",
@@ -31,7 +30,7 @@ setup(
     long_description_content_type='text/markdown',
     url="https://github.com/Miksi-io/Custom-Agent",
     packages=find_packages(include=["miksi_ai_sdk", "miksi_ai_sdk.*"]),
-    ext_modules=cythonize(extensions, build_dir="build"),
+    ext_modules=cythonize(extensions, compiler_directives={'language_level': "3"}),
     install_requires=[
         "langchain", "sqlalchemy", "pymysql",
         "langchain_openai", "openai"
@@ -40,42 +39,7 @@ setup(
     classifiers=[
         # Add your classifiers here
     ],
+    # This line ensures that .py files are not included in your built distributions.
+    # Adjust if you have specific non-Cython .py modules you wish to include.
+    exclude_package_data={'': ['*.py', '*.pyx']},
 )
-'''
-import os
-from os.path import join
-from setuptools import setup, Extension
-from Cython.Build import cythonize
-import glob
-
-# Directory containing your miksisdk package
-directory_path = os.path.dirname(os.path.abspath(__file__))
-miksisdk_path = join(directory_path, 'miksi_ai_sdk')
-
-# Function to get all .py files from miksisdk directory
-def get_py_files(directory):
-    return glob.glob(join(directory, '*.py'))
-
-# Get all .py files to be cythonized
-miksisdk_files = get_py_files(miksisdk_path)
-
-# Create extensions for each .py file
-extensions = [
-    Extension(
-        name=os.path.splitext(os.path.relpath(file, directory_path))[0].replace(os.path.sep, '.'),
-        sources=[file]
-    ) for file in miksisdk_files
-]
-
-setup(
-    name="miksi_ai_sdk",
-    version="0.0.4",
-    install_requires=[
-        "pip", "setuptools", "langchain", "sqlalchemy", "pymysql",
-         "langchain", "langchain_openai","openai", "Cython"
-    ],
-     long_description=open('docs.md').read(),
-    long_description_content_type='text/markdown',
-    ext_modules=cythonize(extensions, language_level="3")
-)
-'''
