383ba5be4682ba1402aecf8478a5c56fef7b299b
RichardKaranuMbuti <Officialforrichardk@gmail.com>
2024-03-30 11:20:36 +0300

    fix upload .py error

--- setup.py
+++ setup.py
@@ -2,31 +2,35 @@ from setuptools import setup, find_packages, Extension
 from Cython.Build import cythonize
 import os
 
-# Dynamically compile all .py files in the miksi_ai_sdk directory
 def find_cython_extensions(package_dir):
     extensions = []
     for root, dirs, files in os.walk(package_dir):
         for file in files:
             if file.endswith(".py"):
-                path = os.path.join(root, file).replace(os.sep, '.').replace('.py', '')
-                ext = Extension(path, [os.path.join(root, file)])
+                # Generating a full path to the py file
+                full_path = os.path.join(root, file)
+                # Removing the .py extension and replacing os separators
+                module_path = full_path[:-3].replace(os.path.sep, '.')
+                ext = Extension(module_path, [full_path])
                 extensions.append(ext)
     return extensions
 
+# Dynamically find extensions
 package_dir = 'miksi_ai_sdk'
 extensions = find_cython_extensions(package_dir)
 
 setup(
     name="miksi-ai-sdk",
-    version="0.0.18",
+    version="0.0.19",
     author="RichardKaranuMbuti",
     author_email="officialforrichardk@gmail.com",
     description="Miksi-AI empowers your BI",
     long_description=open('docs.md').read(),
     long_description_content_type='text/markdown',
     url="https://github.com/Miksi-io/Custom-Agent",
-    packages=find_packages(),
-    ext_modules=cythonize(extensions, language_level = "3"),
+    # Avoiding find_packages() as we're manually specifying extension modules
+    packages=['miksi_ai_sdk'],  # Adjust according to your package structure
+    ext_modules=cythonize(extensions, language_level="3"),
     python_requires='>=3.6',
     install_requires=open('requirements.txt').read().splitlines(),
     classifiers=[
