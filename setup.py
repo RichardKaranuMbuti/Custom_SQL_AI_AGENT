780568242bb6f3984665fdebda2070e573a117b7
RichardKaranuMbuti <Officialforrichardk@gmail.com>
2024-03-30 09:27:53 +0300

    new setup and workflow

--- setup.py
+++ setup.py
@@ -3,23 +3,22 @@ from Cython.Build import cythonize
 import os
 from glob import glob
 
+# Determine the directory in which this setup file resides.
 directory_path = os.path.abspath(os.path.dirname(__file__))
 
 def load_requirements(filename='requirements.txt'):
     with open(os.path.join(directory_path, filename), 'r') as file:
-        return [line.strip() for line in file.readlines()]
+        requirements = [line.strip() for line in file.readlines()]
+    return requirements
 
-# Grab all .py files to compile them into Cython extensions
-module_pyfiles = glob(os.path.join(directory_path, 'miksi_ai_sdk', '*.py'))
-
-# Create Extensions for Cython to compile
+# Automatically compile all .py files in the miksi_ai_sdk directory.
+cython_modules = glob('miksi_ai_sdk/*.py')
 extensions = [
-    Extension(name=os.path.splitext(os.path.relpath(pyfile, directory_path))[0].replace(os.path.sep, '.'),
-              sources=[pyfile]) for pyfile in module_pyfiles
+    Extension(
+        name=os.path.splitext(os.path.basename(py_file))[0],
+        sources=[py_file]
+    ) for py_file in cython_modules
 ]
-
-# Use cythonize on the extensions
-compiled_extensions = cythonize(extensions, compiler_directives={'language_level': "3"})
 
 setup(
     name="miksi-ai-sdk",
@@ -32,6 +31,6 @@ setup(
     url="https://github.com/Miksi-io/Custom-Agent",
     packages=find_packages(),
     install_requires=load_requirements(),
-    python_requires='>=3.6',
-    ext_modules=compiled_extensions,
+    python_requires='>=3.7, <4',
+    ext_modules=cythonize(extensions, compiler_directives={'language_level' : "3"}),
 )
