a3377d7ae103ec4e94eb35733822d4685c88b093
RichardKaranuMbuti <Officialforrichardk@gmail.com>
2024-03-30 09:13:42 +0300

    new setup and workflow

--- setup.py
+++ setup.py
@@ -1,29 +1,28 @@
-from setuptools import setup, find_packages, Extension
+
+from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 import os
-import glob
+import subprocess
 
-# Directory containing your package
-directory_path = os.path.dirname(os.path.abspath(__file__))
-package_path = os.path.join(directory_path, 'miksi_ai_sdk')
+directory_path = '.'
 
-# Function to read the list of requirements from requirements.txt
 def load_requirements(filename='requirements.txt'):
     with open(os.path.join(directory_path, filename), 'r') as file:
         return [line.strip() for line in file.readlines()]
 
-# Function to get all .py files for compilation
-def find_py_files(directory):
-    py_files = []
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith('.py') and file != '__init__.py':
-                module_name = os.path.relpath(os.path.join(root, file), directory).replace(os.path.sep, '.')[:-3]
-                py_files.append((module_name, os.path.join(root, file)))
-    return py_files
+# List of Cython extensions to be compiled
+extensions = [
+    Extension(name="miksi_ai_sdk.agent", sources=["miksi_ai_sdk/agent.py"]),
+    Extension(name="miksi_ai_sdk.api", sources=["miksi_ai_sdk/api.py"]),
+    Extension(name="miksi_ai_sdk.master", sources=["miksi_ai_sdk/master.py"]),
+    Extension(name="miksi_ai_sdk.pythontool", sources=["miksi_ai_sdk/pythontool.py"]),
+    Extension(name="miksi_ai_sdk.sqltool", sources=["miksi_ai_sdk/sqltool.py"]),
+    Extension(name="miksi_ai_sdk.utils", sources=["miksi_ai_sdk/utils.py"]),
+    # Add other extensions as needed
+]
 
-# Create Cython extensions for each .py file
-extensions = [Extension(f"miksi_ai_sdk.{module}", [f]) for module, f in find_py_files(package_path)]
+# Use cythonize on the extensions
+compiled_extensions = cythonize(extensions, compiler_directives={'language_level': "3"})
 
 setup(
     name="miksi-ai-sdk",
@@ -34,11 +33,10 @@ setup(
     long_description=open(os.path.join(directory_path, 'docs.md')).read(),
     long_description_content_type='text/markdown',
     url="https://github.com/Miksi-io/Custom-Agent",
-    packages=find_packages(include=["miksi_ai_sdk", "miksi_ai_sdk.*"]),
-    ext_modules=cythonize(extensions, compiler_directives={'language_level': "3"}),
+    packages=find_packages(),
     install_requires=load_requirements(),
     python_requires='>=3.6',
-    classifiers=[
-        # Add your classifiers here
-    ],
+    ext_modules=compiled_extensions,
+    # It is also common to include package data, which could include compiled .so files,
+    # if they were compiled separately and not as part of the build process.
 )
