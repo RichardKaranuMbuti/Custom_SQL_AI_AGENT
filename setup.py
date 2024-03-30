57b5d5229184cf0141efb904d8cd22d5689ee347
RichardKaranuMbuti <Officialforrichardk@gmail.com>
2024-03-30 09:55:41 +0300

    new setup and workflow

--- setup.py
+++ setup.py
@@ -1,24 +1,20 @@
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, find_packages, Extension
 from Cython.Build import cythonize
 import os
-from glob import glob
 
-# Determine the directory in which this setup file resides.
-directory_path = os.path.abspath(os.path.dirname(__file__))
+# Dynamically compile all .py files in the miksi_ai_sdk directory
+def find_cython_extensions(package_dir):
+    extensions = []
+    for root, dirs, files in os.walk(package_dir):
+        for file in files:
+            if file.endswith(".py"):
+                path = os.path.join(root, file).replace(os.sep, '.').replace('.py', '')
+                ext = Extension(path, [os.path.join(root, file)])
+                extensions.append(ext)
+    return extensions
 
-def load_requirements(filename='requirements.txt'):
-    with open(os.path.join(directory_path, filename), 'r') as file:
-        requirements = [line.strip() for line in file.readlines()]
-    return requirements
-
-# Automatically compile all .py files in the miksi_ai_sdk directory.
-cython_modules = glob('miksi_ai_sdk/*.py')
-extensions = [
-    Extension(
-        name=os.path.splitext(os.path.basename(py_file))[0],
-        sources=[py_file]
-    ) for py_file in cython_modules
-]
+package_dir = 'miksi_ai_sdk'
+extensions = find_cython_extensions(package_dir)
 
 setup(
     name="miksi-ai-sdk",
@@ -26,12 +22,15 @@ setup(
     author="RichardKaranuMbuti",
     author_email="officialforrichardk@gmail.com",
     description="Miksi-AI empowers your BI",
-    long_description=open(os.path.join(directory_path, 'docs.md')).read(),
+    long_description=open('docs.md').read(),
     long_description_content_type='text/markdown',
     url="https://github.com/Miksi-io/Custom-Agent",
     packages=find_packages(),
-    install_requires=load_requirements(),
-    python_requires='>=3.7, <4',
-    compiled_extensions = cythonize(extensions, language_level="3")
-
+    ext_modules=cythonize(extensions, language_level = "3"),
+    python_requires='>=3.6',
+    install_requires=open('requirements.txt').read().splitlines(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
 )
